bdaa68f54d50a563a8054de8c560f2aafd5313d0
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-07-19 19:13:19 +0800

    feat: add tables

--- interactive-comment-section/src/server/db/schema.ts
+++ interactive-comment-section/src/server/db/schema.ts
@@ -3,6 +3,7 @@
 
 import { sql } from "drizzle-orm";
 import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { url } from "inspector";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -11,6 +12,42 @@ import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = sqliteTableCreator((name) => `new_${name}`);
+
+export const comments = createTable("comment", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  content: text("content", { length: 256 }),
+  score: int("score"),
+  authorId: int("author_id"),
+  replyIds: text("reply_ids", { mode: "json" })
+    .$type<number[]>()
+    .default(sql`(json_array())`),
+  createdAt: int("created_at", { mode: "timestamp" })
+    .default(sql`(unixepoch())`)
+    .notNull(),
+  updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const replies = createTable("reply", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  content: text("content", { length: 256 }),
+  score: int("score"),
+  authorId: int("author_id"),
+  replyingTo: int("replying_to"),
+  createdAt: int("created_at", { mode: "timestamp" })
+    .default(sql`(unixepoch())`)
+    .notNull(),
+  updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const users = createTable("user", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  username: text("username", { length: 256 }),
+  avatar: text("avatar", { length: 256 }),
+});
 
 export const posts = createTable(
   "post",
@@ -21,10 +58,10 @@ export const posts = createTable(
       .default(sql`(unixepoch())`)
       .notNull(),
     updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
